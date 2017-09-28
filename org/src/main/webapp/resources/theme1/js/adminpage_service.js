b2611acc78cc70136968d576bc1f34332438f733
Ashok Govinda Gowda <8asokagowda@gmail.com>
2017-09-28 02:12:59 -0500

    Resolving Errors in Calls to Service Layer of Angular JS

--- org/src/main/webapp/resources/theme1/js/adminpage_service.js
+++ org/src/main/webapp/resources/theme1/js/adminpage_service.js
@@ -13,51 +13,61 @@ angular.module('myApp').factory('MovieService', ['$http', '$q', function($http, 
     return factory;
 
     function fetchAllMovies() {
-    	var listOfMovies=[]
+    	var deferred = $q.defer();
         $http.post("/movieInformation/fetchAllMovies",{headers:{'Content-Type': 'application/json'}})
             .then(
             function (response) {
-            	listOfMovies=response.data;
+            	 deferred.resolve(response.data);
             },
             function(errResponse){
                 console.error('Error while fetching Movies');
+                deferred.reject(errResponse);
             }
         );
-        return listOfMovies;
+        return deferred.promise;
     }
 
     function createMovie(movie) {
+    	var deferred = $q.defer();
         movie=JSON.stringify(movie);
         $http.post("/movieInformation/insertMovie",movie)
             .then(
             function (response) {
                 if(response.statusCode=="200"){
                 	console.log("Movie created")
+                	deferred.resolve(response.data);
                 }
             },
             function(errResponse){
                 console.error('Error while creating Movie');
+                deferred.reject(errResponse);
             }
         );
+        return deferred.promise;
     }
 
 
     function updateMovie(movie, id) {
+    	var deferred = $q.defer();
     	movie=JSON.stringify(movie);
         $http.post("/movieInformation/updateMovie",movie)
             .then(
             function (response) {
             	  if(response.statusCode=="200"){
                   	console.log("Movie Updated")
+                  	deferred.resolve(response.data);
                   }
             },
             function(errResponse){
                 console.error('Error while updating Movie');
+                deferred.reject(errResponse);
             }
         );
+        return deferred.promise;
     }
 
     function deleteMovie(id) {
+    	var deferred = $q.defer();
     	var request={
     			"id":id
     	}
@@ -66,12 +76,15 @@ angular.module('myApp').factory('MovieService', ['$http', '$q', function($http, 
             function (response) {
             	if(response.statusCode=="200"){
                   	console.log("Movie Deleted")
+                  	deferred.resolve(response.data);
                   }
             },
             function(errResponse){
                 console.error('Error while deleting Movie');
+                deferred.reject(errResponse);
             }
         );
+    	return deferred.promise;
     }
 
 }]);
